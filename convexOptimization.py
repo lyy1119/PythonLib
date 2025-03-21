8cd25f25e9a43bc710eac6a8e117b8c7165bce58
lyy1119 <lyy2286301015@126.com>
2025-03-21 09:16:10 +0800

    202503210916 暂时保存

--- convexOptimization.py
+++ convexOptimization.py
@@ -1,6 +1,21 @@
 from mathFunction import MathFunction
+from decimal import Decimal
 
-def determine_search_interval(function: MathFunction , x0: list , t0: float , s: list):
+
+def evaluate(queue , x0: MathFunction.DecimalMatrix):
+    '''
+    更新队列中的信息
+    传入一个可迭代对象，collections.deque最佳
+    x0: 初始点
+    queue结构： [[A , S , X , F(X)]]  初始化时令X、F(X)=None
+    '''
+    for i in queue:
+        if i[2] == None:
+            i[2] = x0 + i[0]*i[1] # 计算X=X0+AS
+        if i[3] == None:
+            i[3] = function.evaluate(i[2]) # 计算F(X)
+
+def determine_search_interval(function: MathFunction , x0: MathFunction.DecimalMatrix , t0: float , s: MathFunction.DecimalMatrix):
     '''
     function: 函数
     x0: 起点
@@ -8,4 +23,25 @@ def determine_search_interval(function: MathFunction , x0: list , t0: float , s:
     s: 搜索方向
     return {"float" : [a , b] , "Decimal" : [a , b]}
     '''
-    
+
+    step = Decimal(t0) #步长
+    from collections import deque
+    queue = deque()
+    queue.append([0 , s , None , None])
+    queue.append([step , s , None , None])
+
+    evaluate(queue , x0)
+
+    if queue[1] < queue[0]: # F2 < F1
+        step = -step # 步长反向
+        queue.pop() # 将原A2从末尾出队
+    else:
+        step *= 2
+
+
+    while True: # 进退法
+        pass
+
+    res = list(queue)[0:3:2]
+    res.sort()
+    return res
