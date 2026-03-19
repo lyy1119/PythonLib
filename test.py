627003cecbf4179c4a7af93721b9b936650d7d9e
lycarus <me@lycarus>
2026-03-19 22:17:40 +0800

    修订pyproject中的版本号

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import os
 sys.path.insert(0, os.path.abspath("../my_lib"))  # 指向库所在目录
 # 库的测试程序
 
-from lycarusUtils import *
-from lycarusUtils.mathFunction import *
-from lycarusUtils.convexOptimization import *
+from lycarusLab import *
+from lycarusLab.mathFunction import *
+from lycarusLab.convexOptimization import *
 
 def test_mathfunction():
     print("这是MathFunction类库的测试单元！")
